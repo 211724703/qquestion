7b4e1504184c4da0de960e055ba197544deb9d6d
211724703 <64205224+211724703@users.noreply.github.com>
2021-08-16 17:55:51 +0300

    Update api.js

--- node/routers/api.js
+++ node/routers/api.js
@@ -2,16 +2,15 @@ const router = require('express').Router()
 const user = require('../controller/userControll')
 const question = require('../controller/questions')
 
-router.post('/newUser', user.saveUser)////!!!!!!!!!!!!!!!
-router.post('/updatequestion/:id', question.updateQuestion)
+router.post('/newUser', user.saveUser)
 router.get('/getAllUser', user.getAllUser)
-router.get('/getUser/:name/:password', user.getUser)////!!!!!!
-router.get('/userTasks/:id', user.userQuestions)////!!!!!
+router.get('/getUser/:name/:password', user.getUser)
+router.get('/userTasks/:id', user.userQuestions)
 
 
 
-
-router.post('/newQuestion/:id', question.saveQuestion)////!
+router.post('/updatequestion/:id', question.updateQuestion)
+router.post('/newQuestion/:id', question.saveQuestion)
 router.get('/getAllQuestion', question.getAllQuestion)
 router.get('/getQuestion/:id', question.getQuestion)
 router.delete('/deletequestion/:userId/:id', question.deleteQuestion)
